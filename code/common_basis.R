969ae75c32722b648d9bad2908a7d89319f75803
Eetu Mäkelä <eetu.makela@iki.fi>
2022-05-10 12:12:11 +0300

    add documentation

--- code/common_basis.R
+++ code/common_basis.R
@@ -1,45 +1,57 @@
 library(tidyverse)
 library(DBI)
+library(keyring)
 library(RMariaDB)
 library(pool)
 
-db_passwd <- rstudioapi::askForPassword("Database password")
+if (!("convoy" %in% key_list("convoy"))) key_set("convoy", "convoy")
 
-con <- dbPool(drv=MariaDB(), host = "vm1788.kaj.pouta.csc.fi", dbname = "convoy", user = "convoy", password = db_passwd, bigint = "numeric", load_data_local_infile = T, validationInterval=10)
+con <- tryCatch(
+  dbPool(
+    drv = MariaDB(),
+    host = "vm1788.kaj.pouta.csc.fi",
+    dbname = "convoy",
+    user = "convoy",
+    password = key_get("convoy", "convoy"),
+    bigint = "numeric",
+    load_data_local_infile = T,
+    validationInterval = 10
+  ),
+  error = function(e) {
+    key_delete("convoy", "convoy")
+    stop(e)
+  }
+)
 dbExecute(con, "SET SESSION storage_engine=aria")
-coni <- dbPool(drv=MariaDB(), host = "vm1788.kaj.pouta.csc.fi", dbname = "convoy", user = "convoy", password = db_passwd, bigint = "integer64", load_data_local_infile = T, validationInterval=10)
-dbExecute(coni, "SET SESSION storage_engine=aria")
-rm(db_passwd)
+con_i <- dbPool(drv = MariaDB(), host = "vm1788.kaj.pouta.csc.fi", dbname = "convoy", user = "convoy", password = key_get("convoy", "convoy"), bigint = "integer64", load_data_local_infile = T, validationInterval = 10)
+dbExecute(con_i, "SET SESSION storage_engine=aria")
 
-tweets_c <- tbl(con,"tweets_c")
-tweets_a <- tbl(con,"tweets_a")
-tweet_stats_a <- tbl(con,"tweet_stats_a")
-users_c <- tbl(con,"users_c")
-users_a<- tbl(con,"users_a")
-tweet_hashtags_c <- tbl(con,"tweet_hashtags_c")
-tweet_hashtags_a <- tbl(con,"tweet_hashtags_a")
-tweet_mentions_c <- tbl(con,"tweet_mentions_c")
-tweet_mentions_a <- tbl(con,"tweet_mentions_a") 
-tweet_urls_c <- tbl(con,"tweet_urls_c")
-tweet_urls_a <- tbl(con,"tweet_urls_a") 
-conversations_c <- tbl(con,"conversations_c")
-conversations_a <- tbl(con,"conversations_a")
-ur_conversations_c <- tbl(con,"ur_conversations_c")
-ur_conversations_a <- tbl(con,"ur_conversations_a")
+tweets_c <- tbl(con, "tweets_c")
+tweets_a <- tbl(con, "tweets_a")
+users_c <- tbl(con, "users_c")
+users_a <- tbl(con, "users_a")
+tweet_hashtags_c <- tbl(con, "tweet_hashtags_c")
+tweet_hashtags_a <- tbl(con, "tweet_hashtags_a")
+tweet_mentions_c <- tbl(con, "tweet_mentions_c")
+tweet_mentions_a <- tbl(con, "tweet_mentions_a")
+tweet_urls_c <- tbl(con, "tweet_urls_c")
+tweet_urls_a <- tbl(con, "tweet_urls_a")
+conversations_c <- tbl(con, "conversations_c")
+conversations_a <- tbl(con, "conversations_a")
+ur_conversations_c <- tbl(con, "ur_conversations_c")
+ur_conversations_a <- tbl(con, "ur_conversations_a")
 
-tweets_c_i <- tbl(coni,"tweets_c")
-tweets_a_i <- tbl(coni,"tweets_a")
-tweet_stats_a_i <- tbl(coni,"tweet_stats_a")
-users_c_i <- tbl(coni,"users_c")
-users_a_i <- tbl(coni,"users_a")
-tweet_hashtags_c_i <- tbl(coni,"tweet_hashtags_c")
-tweet_hashtags_a_i <- tbl(coni,"tweet_hashtags_a")
-tweet_mentions_c_i <- tbl(coni,"tweet_mentions_c")
-tweet_mentions_a_i <- tbl(coni,"tweet_mentions_a") 
-tweet_urls_c_i <- tbl(coni,"tweet_urls_c")
-tweet_urls_a_i <- tbl(coni,"tweet_urls_a") 
-conversations_c_i <- tbl(coni,"conversations_c")
-conversations_a_i <- tbl(coni,"conversations_a")
-ur_conversations_c_i <- tbl(coni,"ur_conversations_c")
-ur_conversations_a_i <- tbl(coni,"ur_conversations_a")
-
+tweets_c_i <- tbl(con_i, "tweets_c")
+tweets_a_i <- tbl(con_i, "tweets_a")
+users_c_i <- tbl(con_i, "users_c")
+users_a_i <- tbl(con_i, "users_a")
+tweet_hashtags_c_i <- tbl(con_i, "tweet_hashtags_c")
+tweet_hashtags_a_i <- tbl(con_i, "tweet_hashtags_a")
+tweet_mentions_c_i <- tbl(con_i, "tweet_mentions_c")
+tweet_mentions_a_i <- tbl(con_i, "tweet_mentions_a")
+tweet_urls_c_i <- tbl(con_i, "tweet_urls_c")
+tweet_urls_a_i <- tbl(con_i, "tweet_urls_a")
+conversations_c_i <- tbl(con_i, "conversations_c")
+conversations_a_i <- tbl(con_i, "conversations_a")
+ur_conversations_c_i <- tbl(con_i, "ur_conversations_c")
+ur_conversations_a_i <- tbl(con_i, "ur_conversations_a")
