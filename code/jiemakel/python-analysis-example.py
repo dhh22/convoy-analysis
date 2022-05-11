2cd300567dce7150bada81f464091872b2a4142a
Eetu Mäkelä <eetu.makela@iki.fi>
2022-05-11 14:23:10 +0300

    make the python code a bit more robust again

--- code/jiemakel/python-analysis-example.py
+++ code/jiemakel/python-analysis-example.py
@@ -1,6 +1,7 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(os.getcwd()+"/code")
 import common_basis
 
 con = common_basis.con
