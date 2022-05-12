c5f31e23737cf9aa8d6f57626ff024c0b7d3187b
Eetu Mäkelä <eetu.makela@iki.fi>
2022-05-12 19:10:12 +0300

    fix common_basis loading in jupyter, configure jupytext, add jupyter and jupyterlab into dependencies, and mirror poetry dependencies into conda environment.yml and basic requirements.txt

--- code/jiemakel/.ipynb_checkpoints/python-analysis-example-checkpoint.py
+++ code/jiemakel/.ipynb_checkpoints/python-analysis-example-checkpoint.py
@@ -8,20 +8,13 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
 # ---
 
 # %%
 import os
 import sys
-if "__file__" in globals():
-    sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.append(os.getcwd()+"/code")
-sys.path.append(".")
-sys.path.append("..")
 import common_basis
 
 con = common_basis.con
